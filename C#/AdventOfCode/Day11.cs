bf5d3d4e6b8c88c1acec0f236e50aa331f76dc60
Chris3606 <cridley106@gmail.com>
2024-12-11 07:14:21 -0600

    Day 11 part 1, clearly part 2 will require optimization.

--- C#/AdventOfCode/Day11.cs
+++ C#/AdventOfCode/Day11.cs
@@ -2,14 +2,39 @@
 
 public sealed class Day11 : BaseDay
 {
-    private readonly string _input;
+    private readonly IReadOnlyList<long> _originalNumbers;
 
     public Day11()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _originalNumbers = File.ReadAllText(InputFilePath).ParseList(' ', long.Parse).ToList();
     }
 
-    public override ValueTask<string> Solve_1() => new(_input.Length.ToString());
+    public override ValueTask<string> Solve_1()
+    {
+        var list = _originalNumbers.ToList();
+        var newList = new List<long>();
+        for (int i = 0; i < 25; i++)
+        {
+            foreach (long elem in list)
+            {
+                if (elem == 0)
+                    newList.Add(1);
+                else if (elem.ToString().Length % 2 == 0)
+                {
+                    var digits = elem.ToString();
+                    newList.Add(long.Parse(digits[0..(digits.Length / 2)]));
+                    newList.Add(long.Parse(digits[(digits.Length / 2)..]));
+                }
+                else
+                    newList.Add(elem * 2024);
+            }
+
+            (list, newList) = (newList, list);
+            newList.Clear();
+        }
+
+        return new(list.Count.ToString());
+    }
 
     public override ValueTask<string> Solve_2() => throw new NotImplementedException();
 }
